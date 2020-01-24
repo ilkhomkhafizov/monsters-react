51306d6f69c37555740e90c4f6a64a9722686bfb
ilkhomkhafizov <ilhomhafizov6@gmail.ru>
2020-01-24 22:37:08 +0300

    Updates

---
+++ precache-manifest.921e4c8f2e6d62b4026e666b4b2b813c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b06d7da5406362457a5bc356dc89ca",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "807a340bbe91115257d5",
+    "url": "/monsters-react/static/css/main.d4e3be67.chunk.css"
+  },
+  {
+    "revision": "2cdd72162ba49c7b1131",
+    "url": "/monsters-react/static/js/2.1bb321f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-react/static/js/2.1bb321f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "807a340bbe91115257d5",
+    "url": "/monsters-react/static/js/main.8cc8cad8.chunk.js"
+  },
+  {
+    "revision": "42daab6c8aac7c5320aa",
+    "url": "/monsters-react/static/js/runtime-main.345951e2.js"
+  }
+]);
